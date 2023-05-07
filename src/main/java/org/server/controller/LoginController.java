96a5d9a2cdcc11ea56abc5ac916f162e18d16527
Andrei-Cristian Sandor <andreicsandor@gmail.com>
2023-05-07 11:08:36 +0300

    Add simple Login feature

--- src/main/java/org/server/controller/LoginController.java
+++ src/main/java/org/server/controller/LoginController.java
@@ -1,9 +1,16 @@
 package org.server.controller;
 
+import org.server.dto.LoginDTO;
 import org.server.model.Account;
 import org.server.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class LoginController {
@@ -11,9 +18,22 @@ public class LoginController {
     @Autowired
     private AccountService accountService;
 
-    @PostMapping("/get-credentials")
-    @ResponseBody
-    public Account getUserCredentials(@RequestParam String name, @RequestParam String password) {
-        return this.accountService.getCredentials(name, password);
+    @PostMapping("/login")
+    public ResponseEntity<Map<String, String>> login(@RequestBody LoginDTO loginDTO) {
+        Map<String, String> response = new HashMap<>();
+        try {
+            Account account = accountService.authenticate(loginDTO.getUsername(), loginDTO.getPassword());
+            if (account != null) {
+                response.put("message", "Login successful.");
+                response.put("role", account.getRole());
+                return ResponseEntity.status(HttpStatus.OK).body(response);
+            } else {
+                response.put("message", "Invalid username or password.");
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+            }
+        } catch (AuthenticationException e) {
+            response.put("message", "Login failed.");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+        }
     }
 }
