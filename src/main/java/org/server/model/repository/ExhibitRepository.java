ff8978cc52cf4d05ba7ed6117fb5c620336f0aaa
Andrei-Cristian Sandor <andreicsandor@gmail.com>
2023-05-11 12:19:13 +0300

    Tidy up code

--- src/main/java/org/server/model/repository/ExhibitRepository.java
+++ src/main/java/org/server/model/repository/ExhibitRepository.java
@@ -3,7 +3,6 @@ package org.server.model.repository;
 import org.server.model.Exhibit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,9 +12,6 @@ public interface ExhibitRepository extends JpaRepository<Exhibit, Long> {
 
     @Query("SELECT e FROM Exhibit e ORDER BY e.year")
     List<Exhibit> findAll();
-
-    @Query("SELECT e FROM Exhibit e WHERE e.name = :name AND e.artist = :artist")
-    Exhibit findByNameArtist(@Param("name") String keywordName, @Param("artist") String keywordArtist);
 
     @Query("SELECT e FROM Exhibit e WHERE LOWER(e.name) LIKE %:name%")
     List<Exhibit> findByName(String name);
